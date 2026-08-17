f6f350922edd0304465cf99b3f9517eef5838b39
Chaitanya Hiremath <94546274+QueryCrack@users.noreply.github.com>
2026-08-17 14:24:54 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function showMessage(){
+
+    alert("Thank you for visiting my portfolio!");
+
+}
+
+// Smooth Scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+
+anchor.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});
